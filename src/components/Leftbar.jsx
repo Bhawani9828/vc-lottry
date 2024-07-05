5ef6d460f1f9347c0e29ec9298e18d799bcc758e
SchoolWalaDost <bamanb30@gmail.com>
2024-07-05 18:56:30 +0530

    icon add

--- src/components/Leftbar.jsx
+++ src/components/Leftbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { FaTachometerAlt, FaUserPlus,  FaCalendarAlt, FaFileAlt, FaChartBar, FaCog } from 'react-icons/fa';
-
+import { GrUserAdmin } from "react-icons/gr";
 // eslint-disable-next-line react/prop-types
 function Leftbar({ isMenuOpen, toggleMenu }) {
   return (
@@ -12,17 +12,17 @@ function Leftbar({ isMenuOpen, toggleMenu }) {
         Menu
       </button>
       <div className={`leftbar mt-4 h-screen bg-[#E4572E] duration-300 text-white flex flex-col ${isMenuOpen ? 'block' : 'hidden'} md:block`}>
-        <nav className="flex-1 px-2 space-y-1">
+        <nav className="flex-1 md:mb-32 px-2 space-y-3">
           <Link to="/dashboard" className=" px-4 py-2 rounded-lg hover:bg-[#17BEBB] flex items-center">
             <FaTachometerAlt className="mr-2" />
             Dashboard
           </Link>
           <Link to="/alluser" className=" px-4 py-2 rounded-lg hover:bg-[#17BEBB] flex items-center">
-            <FaFileAlt className="mr-2" />
+            <FaFileAlt className="mr-2 " />
             All User
           </Link>
           <Link to="/addadmin" className=" px-4 py-2 rounded-lg hover:bg-[#17BEBB] flex items-center">
-            <FaUserPlus className="mr-2" />
+            <GrUserAdmin className="mr-2" />
             Add Admin
           </Link>
           <Link to="/adduser" className=" px-4 py-2 rounded-lg hover:bg-[#17BEBB] flex items-center">
